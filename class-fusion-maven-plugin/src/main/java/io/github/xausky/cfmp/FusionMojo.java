96010da980e31f002e499753361450b82f7c512b
xausky <xausky@gmail.com>
2016-11-16 15:44:42 +0800

    [add] default phase.

--- class-fusion-maven-plugin/src/main/java/io/github/xausky/cfmp/FusionMojo.java
+++ class-fusion-maven-plugin/src/main/java/io/github/xausky/cfmp/FusionMojo.java
@@ -12,6 +12,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
@@ -35,7 +36,7 @@ import java.util.TreeSet;
  * 插件入口类.
  * Created by xausky on 10/31/16.
  */
-@Mojo(name = "fusion")
+@Mojo(name = "fusion", defaultPhase = LifecyclePhase.COMPILE)
 public class FusionMojo extends AbstractMojo {
 
     @Component
